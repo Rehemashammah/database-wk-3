c1c14dd75a5ab66166885e6110287dc37d67406a
RehemaShammah <rehemashammah@gmail.com>
2025-09-08 16:17:47 +0300

    Week 3 Submission

--- answers.sql
+++ answers.sql
@@ -1,1 +1,22 @@
+-- Question 1
 
+CREATE TABLE student (
+  id INT PRIMARY KEY,
+  fullName VARCHAR (100),
+  age int
+);
+  
+  
+-- Question 2
+
+INSERT INTO student(id, fullName, age)
+VALUES 
+(1, Alice, 20),
+(2, Robert, 27),
+(3, Sasha, 22);
+
+-- Question 3
+
+UPDATE student
+SET age = 20
+WHERE id = 2;
